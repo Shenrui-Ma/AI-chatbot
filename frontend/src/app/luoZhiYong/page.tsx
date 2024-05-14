3c8fffd6e7b44c419acde512c7fbff37b59093f9
Azrael <202211580372@stu.blcu.edu.cn>
2024-05-14 17:46:51 +0800

    added Luo

--- frontend/src/app/luoZhiYong/page.tsx
+++ frontend/src/app/luoZhiYong/page.tsx
@@ -1,20 +1,35 @@
+"use client";
 import React, { useState } from "react";
 import InputComponent from "@/components/InputComponent";
 import ImageDisplayComponent from "@/components/ImageDisplayComponent";
 import WordsAfterClick from "@/components/WordsAfterClick";
+import { MessageProvider } from "@/components/MessageContext";
+import OutputDisplayComponent from "@/components/OutputDisplayComponent";
+import Sidebar from "@/components/item-sidebar";
+import ItemAuthor from "@/components/item-author";
+import ButtonShare from "@/components/button-share";
 
-const Chat = () => {
+export default function Test() {
+  const imagePath = "/images/罗志勇2.png";
+
   return (
-    // 测试：先随便展示张院长图片
-    <div className="flex flex-col items-center justify-center h-[100vh]">
-      <img
-        src="/images/罗志勇2.png"
-        alt="帅气の罗志勇"
-        className="w-[300px] h-[400px]"
-      />
-      <InputComponent />
-    </div>
+    <MessageProvider>
+      {" "}
+      {/* 使用MessageProvider包裹组件 */}
+      <div className="flex relative">
+        <div className="flex flex-col justify-center items-center w-full">
+          <div className="relative w-full flex justify-center">
+            <ImageDisplayComponent src={imagePath} />
+            <div className="absolute right-40 top-20">
+              <OutputDisplayComponent />
+            </div>
+          </div>
+          <InputComponent character={"LuoZhiyong"} />
+        </div>
+        <ItemAuthor />
+        <ButtonShare />
+        <Sidebar />
+      </div>
+    </MessageProvider>
   );
-};
-
-export default Chat;
+}
