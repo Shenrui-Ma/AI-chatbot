4b2c433fdc12ba6c24a41ec939cf4a70cba5b898
Azrael <202211580372@stu.blcu.edu.cn>
2024-05-21 15:17:19 +0800

    added chathistory with YuDong

--- frontend/src/app/YuDong/page.tsx
+++ frontend/src/app/YuDong/page.tsx
@@ -9,43 +9,9 @@ import Sidebar from "@/components/item-sidebar";
 import ItemAuthor from "@/components/item-author";
 import ButtonShare from "@/components/button-share";
 import Certificates from "@/components/item-certificates";
+import ChatHistory from "@/components/ChatHistory";
 
-const awards = [
-  {
-    award: "羽毛球打得最好奖",
-  },
-  {
-    award: "以前教过高数奖",
-  },
-  {
-    award: "以前教过C语言奖",
-  },
-  {
-    award: "教课外号C罗奖",
-  },
-  {
-    award: "上课看手机学生最少奖",
-  },
-  {
-    award: "机器学习不难奖",
-  },
-  {
-    award: "家里孩子只看过滤版抖音奖",
-  },
-  {
-    award: "校门口人脸识别技术窃取奖",
-  },
-  {
-    award: "教过日语学生奖",
-  },
-  ...Array(0).fill({
-    userName: "Waiting...",
-    comment: "nothing ...",
-    avatarUrl: "/images/Silver_wolf.png",
-  }),
-];
-
-export default function Test() {
+export default function YuDong() {
   const imagePath = "/images/YuDong.png";
 
   return (
@@ -55,17 +21,12 @@ export default function Test() {
       <div className="flex relative min-h-screen">
         <div className="flex flex-col items-center justify-center w-full">
           <div className="relative flex justify-center items-center w-full">
-            <div className="absolute left-0 flex flex-col items-center">
-              {awards.map((user, index) => (
-                <div
-                  key={index}
-                  className="mb-4 flex justify-center relative left-[300px]"
-                >
-                  <Certificates award={user.award} />
-                </div>
-              ))}
+            <div className="flex flex-col relative right-[18vh] bottom-20">
+              <ChatHistory />
             </div>
-            <ImageDisplayComponent src={imagePath} />
+            <div className="flex flex-col relative right-[14vh]">
+              <ImageDisplayComponent src={imagePath} />
+            </div>
             <div className="absolute right-20 top-10">
               <OutputDisplayComponent />
             </div>
